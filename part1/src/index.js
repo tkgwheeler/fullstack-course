7f04045732e4198c335fdab03c3db83583cddd89
buckeyeburl <tkgwheeler@hotmail.co.uk>
2019-07-29 21:33:36 +0100

    upto exercise 1.8

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Statistics = props => {
+  const statGood = props.good;
+  const statNeutral = props.neutral;
+  const statBad = props.bad;
+  const allStats = +statGood + statNeutral + statBad;
+  const averageStats = (statGood - statBad) / allStats;
+  const percentGood = (statGood / allStats) * 100;
+
+  return (
+    <div>
+      <h1> Statistics</h1>
+      <p>Good:{statGood}</p>
+      <p>Neutral:{statNeutral}</p>
+      <p>Bad:{statBad}</p>
+      <p>All:{allStats}</p>
+      <p>Average:{averageStats.toFixed(2)}</p>
+      <p>Satisfied:{percentGood.toFixed(2)}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const allStats = +good + neutral + bad;
-  const averageStats = (good - bad) / allStats;
-  const percentGood = (good / allStats) * 100;
-
   return (
     <div>
       <h1>Give Feedback</h1>
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
-      <h1> Statistics</h1>
-      <p>Good:{good}</p>
-      <p>Neutral:{neutral}</p>
-      <p>Bad:{bad}</p>
-      <p>All:{allStats}</p>
-      <p>Average:{averageStats.toFixed(4)}</p>
-      <p>Satisfied:{percentGood.toFixed(4)}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
