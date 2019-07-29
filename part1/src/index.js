c24e1da660b567db07fc49b0c514080765653cbf
buckeyeburl <tkgwheeler@hotmail.co.uk>
2019-07-29 22:14:00 +0100

    1.10 added button components

--- part1/src/index.js
+++ part1/src/index.js
@@ -39,17 +39,33 @@ const Statistic = props => {
   );
 };
 
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const updateGood = () => {
+    setGood(good + 1);
+  };
+
+  const updateNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const updateBad = () => {
+    setBad(bad + 1);
+  };
+
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Button onClick={updateGood} text="Good" />
+      <Button onClick={updateNeutral} text="Neutral" />
+      <Button onClick={updateBad} text="Bad" />
       <h1>Statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
